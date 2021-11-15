a908f9e54c0c5d4123459fb4dec80c8a294f4b51
uhuru rawlings <rawlings.huru@student.moringaschool.com>
2021-11-15 10:38:34 +0300

    changed the formular

--- README.md
+++ README.md
@@ -4,16 +4,19 @@
     The app day of birth is an app that calculates the day of the week.
     it uses your day of birth to get the to get the day you were born.
 ### Languages used.
-###### HTML
+#### HTML
   I used html to show the web content.
-###### CSS
+#### CSS
    I used css to style the HTML content
-###### Javascript
+#### Javascript
    I used javascript to calculate and 
-###### Bootstrap
+#### Bootstrap
    I used bootstrap to style the content
 
 ### SetUp Information
   Setting up this project you just need to download the zip file of this project from my github account. 
   Extract the file.
   You can double click on the index.html file to open it in a browser or you can open it with any text editor to view the code.
+
+### For queries you can contact me 
+   uhururawlings40@gmail.com
--- index.js
+++ index.js
@@ -6,15 +6,9 @@ const validates = () => {
         return false;
     }
     else{
-        var alldates = dobs.split('-');
-        var year = parseInt(alldates[0]);
-        var month = parseInt(alldates[1]);
-        var days = parseInt(alldates[2]);
-        var yearusable = parseInt(alldates[0].substr(2,4));
-        var leapyear = Math.floor((year/100) + 1);
-        var exactday = ( ( (leapyear/4) -2*leapyear-1) + ((5*yearusable/4) ) + ((26*(month+1)/10)) + days )%7;
-        var indexes = Math.floor(exactday);
-         
+     
+        var indexes = new Date(dobs).getDay();
+      
         var males = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
         var females = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
         var daysoftheweek = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
